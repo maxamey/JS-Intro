8e0d5c3538f54896c725e885af57703d226b2a1f
Max Amey <maxwellamey@gmail.com>
2016-08-08 13:41:03 -0500

    Finished JS-Intro - Max

---
+++ javascripts/main.js
@@ -0,0 +1,71 @@
+/*=========================================================
+    Part 1: Variables
+*/
+var givenName;
+// Q: What does `givenName` equal right now?
+// A: Nothing
+
+
+givenName = "Tim";
+// Q: What is `givenName` set to right now?
+// A: Tim
+
+givenName = "JD";
+// Q: What is `givenName` set to right now?
+// A: JD
+
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+// A: Hello, how are you?
+
+
+/*=========================================================
+     Part 2: Simple Math
+*/
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+// A: 40
+
+console.log(math);
+
+math = high - "5";
+// Q: What is `math` set to?
+// A: 45
+
+console.log(math);
+
+/* =========================================================
+     Part 3: Expressions
+*/
+
+// Create a variable to calculate Tim's age
+// The answer should read "Tim is 32 years old"
+// The answer shoud not be written in a comment.
+var born = 1983;
+var today = 2015;
+
+// Answers Below:
+var age = "Tim is " + (today - born) + " years old";
+
+console.log(age);
+
+// Store some information following in variables.
+var yourName;
+var instructorName;
+
+// Update the variables above so the expression reads correctly.
+// Answers Below:
+
+var yourName = "Max";
+var instructorName = "Jason";
+
+// Final Statement
+var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+
+console.log(statement);
